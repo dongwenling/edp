1c816b9e840b66b7e89a56c39d6c1d818fd7ef8a
dongwenling <670030175@qq.com>
2020-02-23 19:16:50 +0800

    first com

---
+++ src/main/java/Mp.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Mp {
+    public static void main(String args[]){
+        int[] arr=new int[4];
+        System.out.println("请输入几位数，并对其进行冒泡排序！");
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<4;i++) {
+            int s = sc.nextInt();
+            arr[i]=s;
+        }
+        for(int n=0;n<3;n++){
+            for(int m=0;m<arr.length-1-n;m++){
+                if(arr[m]>arr[m+1]){
+                    int tmp=arr[m];
+                    arr[m]=arr[m+1];
+                    arr[m+1]=tmp;
+                }
+
+            }
+        }
+        for(int k=0;k<4;k++){
+            System.out.print(arr[k]+" ");
+        }
+
+    }
+}
