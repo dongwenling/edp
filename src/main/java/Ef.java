197bb7041bdd99afb41333b5e8ef905d07f30e27
dongwenling <670030175@qq.com>
2020-02-24 22:25:38 +0800

    加注释

--- src/main/java/Ef.java
+++ src/main/java/Ef.java
@@ -17,7 +17,7 @@ public class Ef {
             if (arr[mid] > key) {
                 max =mid-1;
             }else if (arr[mid]<key){
-                min=mid+1;
+                min=mid+1;K
             }else {
                 System.out.println("有key:"+arr[mid]+"下标为："+mid);
                 System.exit(0);
